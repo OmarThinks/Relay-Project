6558162425ae8150bbcabd4cd8af726eea9dfe60
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-10-09 10:05:10 +0300

    HoverCard: prerendered on hover

--- relay-examples/newsfeed/src/components/PosterByline.tsx
+++ relay-examples/newsfeed/src/components/PosterByline.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
 import { useRef } from "react";
-import Image from "./Image";
+import { useFragment, useQueryLoader } from "react-relay";
 import { graphql } from "relay-runtime";
-import { useFragment } from "react-relay";
-import { PosterBylineFragment$key } from "./__generated__/PosterBylineFragment.graphql";
 import Hovercard from "./Hovercard";
-import PosterDetailsHovercardContents from "./PosterDetailsHovercardContents";
+import Image from "./Image";
+import PosterDetailsHovercardContents, {
+  PosterDetailsHovercardContentsQuery,
+} from "./PosterDetailsHovercardContents";
+import { PosterBylineFragment$key } from "./__generated__/PosterBylineFragment.graphql";
+import type { PosterDetailsHovercardContentsQuery as HovercardQueryType } from "./__generated__/PosterDetailsHovercardContentsQuery.graphql";
 
 const PosterBylineFragment = graphql`
   fragment PosterBylineFragment on Actor {
@@ -29,6 +32,12 @@ export default function PosterByline({ poster }: Props): React.ReactElement {
   const data = useFragment(PosterBylineFragment, poster);
   const hoverRef = useRef(null);
 
+  const [hovercardQueryRef, loadHovercardQuery] =
+    useQueryLoader<HovercardQueryType>(PosterDetailsHovercardContentsQuery);
+  function onBeginHover() {
+    loadHovercardQuery({ posterID: data.id });
+  }
+
   return (
     <div ref={hoverRef} className="byline">
       <Image
@@ -38,8 +47,8 @@ export default function PosterByline({ poster }: Props): React.ReactElement {
         className="byline__image"
       />
       <div className="byline__name">{data.name}</div>
-      <Hovercard targetRef={hoverRef}>
-        <PosterDetailsHovercardContents posterID={data.id} />
+      <Hovercard targetRef={hoverRef} onBeginHover={onBeginHover}>
+        <PosterDetailsHovercardContents queryRef={hovercardQueryRef} />
       </Hovercard>
     </div>
   );
