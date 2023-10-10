66508e5eaa7446673295ccde04658044388bffd8
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-10-10 10:17:13 +0300

    ContactsList: used transition

--- relay-examples/newsfeed/src/components/ContactsList.tsx
+++ relay-examples/newsfeed/src/components/ContactsList.tsx
@@ -1,12 +1,10 @@
-import * as React from "react";
+import { useState, useTransition } from "react";
+import { useRefetchableFragment } from "react-relay";
 import { graphql } from "relay-runtime";
-import { useFragment } from "react-relay";
-import type { ContactsListFragment$key } from "./__generated__/ContactsListFragment.graphql";
 import Card from "./Card";
 import ContactRow from "./ContactRow";
 import SearchInput from "./SearchInput";
-import { useRefetchableFragment } from "react-relay";
-import { useState } from "react";
+import type { ContactsListFragment$key } from "./__generated__/ContactsListFragment.graphql";
 
 export type Props = {
   viewer: ContactsListFragment$key;
@@ -24,18 +22,25 @@ const ContactsListFragment = graphql`
 `;
 
 export default function ContactsList({ viewer }: Props) {
+  const [isPending, startTransition] = useTransition();
   const [data, refetch] = useRefetchableFragment(ContactsListFragment, viewer);
 
   const [searchString, setSearchString] = useState("");
   const onSearchStringChanged = (value: string) => {
     setSearchString(value);
-    refetch({ search: value });
+    startTransition(() => {
+      refetch({ search: value });
+    });
   };
 
   return (
     <Card dim={true}>
       <h3>Contacts</h3>
-      <SearchInput value={searchString} onChange={onSearchStringChanged} />
+      <SearchInput
+        value={searchString}
+        onChange={onSearchStringChanged}
+        isPending={isPending}
+      />
       {data.contacts.map((contact) => (
         <ContactRow key={contact.id} contact={contact} />
       ))}
